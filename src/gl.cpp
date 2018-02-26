76b43cbd7218b73fe6a448dce6b231526edd832a
jucom <julien.combattelli@hotmail.com>
2018-02-26 20:43:53 +0100

    Update main program to show a ligh example
    Signed-off-by: jucom <julien.combattelli@hotmail.com>

--- src/gl.cpp
+++ src/gl.cpp
@@ -14,6 +14,50 @@
 #include "Model.hpp"
 #include "Shader.hpp"
 
+float cube[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+};
+
 int main()
 {
 	sf::ContextSettings settings;
@@ -24,10 +68,13 @@ int main()
 	settings.minorVersion = 3;
 	settings.attributeFlags = sf::ContextSettings::Core;
 
-	const GLuint width = 640;
-	const GLuint height = 480;
+	const GLuint width = 1024;
+	const GLuint height = 768;
 
-    sf::Window window(sf::VideoMode(width, height), "SFML window with OpenGL", sf::Style::Default, settings);
+    sf::Window window(sf::VideoMode(width, height), "SFML window with OpenGL", sf::Style::Fullscreen, settings);
+    window.setMouseCursorVisible(false);
+    sf::Mouse::setPosition({width/2, height/2});
+    window.setVerticalSyncEnabled(true);
 
     glewExperimental = true;
     if (glewInit() != GLEW_OK)
@@ -44,12 +91,50 @@ int main()
 
     glViewport(0, 0, window.getSize().x, window.getSize().y);
 
-    Camera cam(glm::vec3{0,0,3});
-    Shader ourShader("shaders/simple.vert", "shaders/simple.frag");
-    Model ourModel("./suzanne.obj");
+    Camera camera(glm::vec3{0,0,3});
+    Shader lightingShader("shaders/lighting.vert", "shaders/lighting.frag");
+    Shader lampShader("shaders/lamp.vert", "shaders/lamp.frag");
 
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+
+    glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+    // first, configure the cube's VAO (and VBO)
+    unsigned int VBO, cubeVAO;
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+
+    glBindVertexArray(cubeVAO);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // normal attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+
+    // second, configure the light's VAO (VBO stays the same; the vertices are the same for the light object which is also a 3D cube)
+    unsigned int lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // note that we update the lamp's position attribute's stride to reflect the updated buffer data
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    sf::Clock clock;
     while (window.isOpen())
     {
+    	float currentFrame = clock.getElapsedTime().asSeconds();
+    	deltaTime = currentFrame - lastFrame;
+    	lastFrame = currentFrame;
+
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -61,18 +146,57 @@ int main()
                 glViewport(0, 0, event.size.width, event.size.height);
         }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+        	camera.ProcessKeyboard(FORWARD, deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            camera.ProcessKeyboard(BACKWARD, deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+            camera.ProcessKeyboard(LEFT, deltaTime);
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        	camera.ProcessKeyboard(RIGHT, deltaTime);
 
 
-        ourShader.use();
-		glm::mat4 projection = glm::perspective(glm::radians(cam.Zoom), (float)width / (float)height, 0.1f, 100.0f);
-		glm::mat4 view = cam.GetViewMatrix();
-		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
-		glm::mat4 model = glm::mat4(1);
-		ourShader.setMat4("model", model);
-        ourModel.Draw(ourShader);
+		camera.ProcessMouseMovement(
+				sf::Mouse::getPosition(window).x - width/2.0f,
+				height/2.0f - sf::Mouse::getPosition(window).y,
+				false);
+		sf::Mouse::setPosition({width/2, height/2}, window);
 
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        // be sure to activate shader when setting uniforms/drawing objects
+		lightingShader.use();
+		lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightingShader.setVec3("lightPos", lightPos);
+
+		// view/projection transformations
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)width / (float)height, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
+
+		// world transformation
+		glm::mat4 model;
+		lightingShader.setMat4("model", model);
+
+		// render the cube
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		// also draw the lamp object
+		lampShader.use();
+		lampShader.setMat4("projection", projection);
+		lampShader.setMat4("view", view);
+		model = glm::mat4();
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+		lampShader.setMat4("model", model);
+
+		glBindVertexArray(lightVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
         window.display();
     }
