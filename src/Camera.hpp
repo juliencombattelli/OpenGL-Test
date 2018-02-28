c49eb9b21e5f667fc458ea22c076fdc8f51e2381
jucom <julien.combattemlli@hotmail.com>
2018-02-28 16:36:31 +0100

    Update init values for Camera class
    Signed-off-by: jucom <julien.combattemlli@hotmail.com>

--- src/Camera.hpp
+++ src/Camera.hpp
@@ -12,14 +12,16 @@ enum Camera_Movement {
     FORWARD,
     BACKWARD,
     LEFT,
-    RIGHT
+    RIGHT,
+    UP,
+    DOWN
 };
 
 // Default camera values
 const float YAW         = -90.0f;
-const float PITCH       =  0.0f;
-const float SPEED       =  2.5f;
-const float SENSITIVITY =  0.1f;
+const float PITCH       =   0.0f;
+const float SPEED       =   2.5f;
+const float SENSITIVITY =   0.1f;
 const float ZOOM        =  45.0f;
 
 
