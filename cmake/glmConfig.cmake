8d9b3a344e051a8b6668cf36d7b0df554112feb7
jucom <julien.combattelli@hotmail.com>
2018-02-28 23:10:43 +0100

    Add glm sources in external folder as v0.9.8.x is required but not available in ubuntu 16.04
    Signed-off-by: jucom <julien.combattelli@hotmail.com>

--- cmake/glmConfig.cmake
+++ cmake/glmConfig.cmake
@@ -1,5 +1,5 @@
 set(GLM_VERSION "0.9.7")
-set(GLM_INCLUDE_DIRS "/tmp/mozilla_jucom0/glm-0.9.7.2")
+set(GLM_INCLUDE_DIRS "../external/glm-0.9.8.5")
 
 if (NOT CMAKE_VERSION VERSION_LESS "3.0")
     include("${CMAKE_CURRENT_LIST_DIR}/glmTargets.cmake")
